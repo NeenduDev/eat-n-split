e270310dcd24579cf2122fbc5fab44f25460d9b4
NeenduDev <neendu.wickremasinghe@gmail.com>
2025-01-05 21:59:33 +0530

    Build static UI for the FriendList component with initial styling and structure

--- src/App.js
+++ src/App.js
@@ -20,5 +20,42 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <div className="app">
+      <FriendList />
+    </div>
+  );
+}
+
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <div className="sidebar">
+      <ul>
+        {friends.map((friend) => (
+          <Friend friend={friend} key={friend.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      <p
+        className={
+          friend.balance > 0 ? `green` : friend.balance < 0 ? `red` : null
+        }
+      >
+        {friend.balance > 0 && `${friend.name} ows you ${friend.balance}€`}
+        {friend.balance < 0 &&
+          `you owe ${friend.name} ${Math.abs(friend.balance)}€`}
+        {friend.balance === 0 && ` you and ${friend.name} are even`}
+      </p>
+      <button className="button">Select</button>
+    </li>
+  );
 }
